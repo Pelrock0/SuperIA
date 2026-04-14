b4723f01d7f934cb8afb0c595be96ae9c096b333
Pelrock0 <pelrock@gmail.com>
2026-04-14 16:56:59 +0200

    Add middleware to trust proxies in app.php for improved request handling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->redirectGuestsTo(function ($request) {
             if ($request->is('admin') || $request->is('admin/*')) {
                 return route('backpack.auth.login');

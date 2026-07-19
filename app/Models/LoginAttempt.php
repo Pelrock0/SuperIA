2cbe23959b568df95101f7727226e820280bede9
Pelrock0 <pelrock@gmail.com>
2026-07-19 20:35:24 +0200

    feat: enable Backpack CrudTrait on LoginAttempt and ProductoHistorial

--- app/Models/LoginAttempt.php
+++ app/Models/LoginAttempt.php
@@ -1,11 +1,14 @@
 <?php
 
 namespace App\Models;
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
 
 use Illuminate\Database\Eloquent\Model;
 
 class LoginAttempt extends Model
 {
+    use CrudTrait;
+    
     public $timestamps = false;
 
     protected $fillable = [
